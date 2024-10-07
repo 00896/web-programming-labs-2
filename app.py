14ca6892b29b8f73e374d835462c5634f209f1ee
Лидия Кобзева <kobzewa.lida@yandex.ru>
2024-10-07 14:04:23 +0300

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -486,4 +486,7 @@ def add_flower(name):
 @app.route('/lab2/example')
 def example():
     name = 'Кобзева Лидия'
-    return render_template('example.html', name=name)
+    number = '2'
+    group = 'ФБИ-21'
+    number_course = '3'
+    return render_template('example.html', name=name, number=number, group=group, number_course=number_course)
