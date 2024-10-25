93227a828b7341072fc0a8d91ff88da625b83173
Лидия Кобзева <kobzewa.lida@yandex.ru>
2024-10-25 10:38:33 +0300

    Настройки отображения

--- lab3.py
+++ lab3.py
@@ -66,3 +66,53 @@ def pay():
 def success():
     price = request.args.get('price')
     return render_template('lab3/success.html', price=price)
+
+
+@lab3.route('/lab3/settings')
+def settings():
+    color = request.args.get('color')
+    background_color = request.args.get('background_color')
+    font_size = request.args.get('font_size')
+    font_weight = request.args.get('font_weight')  
+
+    # если параметры переданы, устанавливаем их в куки
+    if color or background_color or font_size or font_weight:
+        resp = make_response(redirect('/lab3/settings'))
+
+        if color:
+            resp.set_cookie('color', color)  
+        if background_color:
+            resp.set_cookie('background_color', background_color) 
+        if font_size:
+            resp.set_cookie('font_size', font_size)  
+        if font_weight:
+            resp.set_cookie('font_weight', font_weight) 
+
+        return resp  
+
+    #в случае, если параметры не переданы, получаем их из куки
+    color = request.cookies.get('color')
+    background_color = request.cookies.get('background_color')
+    font_size = request.cookies.get('font_size')
+    font_weight = request.cookies.get('font_weight')  
+
+    resp = make_response(render_template(
+        'lab3/settings.html', 
+        color=color, 
+        background_color=background_color, 
+        font_size=font_size,
+        font_weight=font_weight  
+    ))
+    return resp 
+
+
+@lab3.route('/lab3/clear_settings')
+def clear_settings():
+    resp = make_response(redirect('/lab3/settings'))
+
+    resp.delete_cookie('color')
+    resp.delete_cookie('background_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('font_weight')
+
+    return resp
