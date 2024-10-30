3b14327c3827592e4fe47b3b2ab11d4f9817568d
Лидия Кобзева <kobzewa.lida@yandex.ru>
2024-10-30 13:03:08 +0300

    Арифметика

--- lab4.py
+++ lab4.py
@@ -26,3 +26,84 @@ def div():
         return render_template('lab4/div.html', error='На ноль делить нельзя!')
     result = x1 / x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('lab4/sum-form.html')
+
+
+@lab4.route('/lab4/sum', methods=['POST'])
+def sum():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    #если какое-либо из полей пустое, то считать, что в него как будто бы ввели 0
+    x1 = int(x1) if x1 else 0
+    x2 = int(x2) if x2 else 0
+    
+    result = x1 + x2
+    return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/multiply-form')
+def multiply_form():
+    return render_template('lab4/multiply-form.html')
+
+
+@lab4.route('/lab4/multiply', methods=['POST'])
+def multiply():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    #если какое-либо из полей пустое, то считать, что в него как будто бы ввели 1
+    x1 = int(x1) if x1 else 1
+    x2 = int(x2) if x2 else 1
+    
+    result = x1 * x2
+    return render_template('lab4/multiply.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('lab4/sub-form.html')
+
+
+@lab4.route('/lab4/sub', methods=['POST'])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    #выдавать ошибку при пустых полях
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub.html', error='Оба поля должны быть заполненными!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    
+    result = x1 - x2
+    return render_template('lab4/sub.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/expo-form')
+def expo_form():
+    return render_template('lab4/expo-form.html')
+
+
+@lab4.route('/lab4/expo', methods=['POST'])
+def expo():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    #выдавать ошибку при пустых полях, а также если оба поля равны нулю
+    if x1 == '' or x2 == '':
+        return render_template('lab4/expo.html', error='Оба поля должны быть заполненными!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    
+    if x1 == 0 and x2 == 0:
+        return render_template('lab4/expo.html', error='0 в степени 0 не определено!')
+
+    result = x1 ** x2
+    return render_template('lab4/expo.html', x1=x1, x2=x2, result=result)
