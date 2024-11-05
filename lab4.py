76e0885bafba69791763be91a3028a4b1febc9d0
Лидия Кобзева <kobzewa.lida@yandex.ru>
2024-11-05 20:15:35 +0300

    Холодильник

--- lab4.py
+++ lab4.py
@@ -168,3 +168,32 @@ def login():
 def logout():
     session.pop('login', None)
     return redirect('/lab4/login')
+
+
+@lab4.route('/lab4/fridge', methods=['GET', 'POST'])
+def fridge():
+    message = ""
+    snowflakes = ""
+    
+    if request.method == 'POST':
+        try:
+            temperature = float(request.form.get('temperature'))
+        except (TypeError, ValueError):
+            message = "Ошибка: не задана температура"
+            return render_template('lab4/fridge.html', message=message, snowflakes=snowflakes)
+
+        if temperature < -12:
+            message = "Не удалось установить температуру — слишком низкое значение"
+        elif temperature > -1:
+            message = "Не удалось установить температуру — слишком высокое значение"
+        elif -12 <= temperature <= -9:
+            message = f"Установлена температура: {temperature}°С"
+            snowflakes = "❄️❄️❄️"  
+        elif -8 <= temperature <= -5:
+            message = f"Установлена температура: {temperature}°С"
+            snowflakes = "❄️❄️"  
+        elif -4 <= temperature <= -1:
+            message = f"Установлена температура: {temperature}°С"
+            snowflakes = "❄️"  
+
+    return render_template('lab4/fridge.html', message=message, snowflakes=snowflakes)
