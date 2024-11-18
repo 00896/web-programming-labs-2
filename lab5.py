5a81a2ccee2a25a4b5d772329e85ae4d12f661f1
Лидия Кобзева <kobzewa.lida@yandex.ru>
2024-11-18 17:42:34 +0300

    Улучшения

--- lab5.py
+++ lab5.py
@@ -9,6 +9,22 @@ from psycopg2.extras import RealDictCursor
 def lab():
     return render_template('lab5/lab5.html', login=session.get('login'))
 
+def db_connect():
+    # для работы с БД нам нужно сначала подключиться к БД, затем получить курсор
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'lidia_kobzeva_knowledge_base',
+        user = 'lidia_kobzeva_knowledge_base',
+        password = 'dfgmoi45'
+    )
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+
+    return conn, cur
+
+def db_close(conn,cur):
+    conn.commit()
+    cur.close()
+    conn.close()
 
 @lab5.route('/lab5/register', methods= ['GET', 'POST'])
 def register():
@@ -21,29 +37,19 @@ def register():
     if not (login or password):
         return render_template('lab5/register.html', error = 'Заполните все поля')
     
-    # для работы с БД нам нужно сначала подключиться к БД, затем получить курсор
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'lidia_kobzeva_knowledge_base',
-        user = 'lidia_kobzeva_knowledge_base',
-        password = 'dfgmoi45'
-    )
-    cur = conn.cursor()
+    conn, cur = db_connect()
 
     # далее сделаем SQL-запрос к БД, поищем пользователя с введённым логином
     cur.execute(f"SELECT login FROM users WHERE login='{login}';")
     # fetchone() для получения результатов запроса
     if cur.fetchone():
         # перед тем, как выйти из обработчика по команде return, надо закрыть БД и курсор, иначе будут утечки памяти
-        cur.close()
-        conn.close()
+        db_close(conn,cur)
         return render_template('lab5/register.html', error = 'Такой пользователь уже существует')
     
     # если же пользователя в БД нет, то его можно зарегистрировать в системе, вставив в таблицу логин и пароль
     cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
-    conn.commit()
-    cur.close()
-    conn.close()
+    db_close(conn,cur)
     return render_template ('lab5/success.html', login=login )
 
 
@@ -58,29 +64,20 @@ def login():
     if not (login or password):
         return render_template('lab5/login.html', error = 'Заполните все поля')
     
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'lidia_kobzeva_knowledge_base',
-        user = 'lidia_kobzeva_knowledge_base',
-        password = 'dfgmoi45'
-    )
-    cur = conn.cursor(cursor_factory= RealDictCursor)
+    conn, cur = db_connect()
 
     cur.execute(f"SELECT * FROM users WHERE login='{login}';")
     user = cur.fetchone()
 
     if not user:
-        cur.close()
-        conn.close()
+        db_close(conn,cur)
         return render_template('lab5/login.html', error = 'Логин и/или пароль неверен')
     
     # если пользователь найден, то нужно прочитать его пароль — если введённый пароль не совпадает с паролем из БД, то выдать сообщение об ошибке
     if user['password'] !=password:
-        cur.close()
-        conn.close()
+        db_close(conn,cur)
         return render_template('lab5/login.html', error = 'Логин и/или пароль неверен')
     
     session['login'] = login
-    cur.close()
-    conn.close()
+    db_close(conn,cur)
     return render_template('lab5/success_login.html', login=login)
